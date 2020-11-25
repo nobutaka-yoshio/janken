9d723dedb10480c252a054ed854a08c52985713e
nobutaka-yoshio <75005442+nobutaka-yoshio@users.noreply.github.com>
2020-11-25 15:18:34 +0900

    編集後

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,31 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
+import discord
+import random
+import asyncio
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'Nzc5OTg5ODE2ODUwMjUxNzk3.X7okHw.OrX96-McjoMWFobX7sZrzK93s2U'
+arr = ['ぐー:punch:', 'ちょき:v:', 'ぱー:hand_splayed:']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+
+    if message.content == 'じゃんけん':
+        await message.channel.send(message.author.mention+" は「"+random.choice(arr)+"」を出しました。")
 
 
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
